ad81629f554f98442a45c17987dbc74c7e4fe303
Dexer Matters <dexermatters@gmail.com>
2025-11-23 16:09:08 +0800

    Implement System F with type and term definitions, including shift, substitution, and renaming functions

---
+++ LearningLC/SystemF/Basic.lean
@@ -0,0 +1,81 @@
+import LearningLC.DeBruijn
+
+inductive Ty : Nat → Type u where
+  | tyVar {Δ} : Fin Δ → Ty Δ
+  | tyNat {Δ} : Ty Δ
+  | tyArr {Δ} : Ty Δ → Ty Δ → Ty Δ
+  | tyForall {Δ} : Ty (Δ + 1) → Ty Δ
+
+def Ty.shift {Δ} : Ty Δ → Ty (Δ + 1)
+  | .tyVar x      => .tyVar (Fin.succ x)
+  | .tyNat        => .tyNat
+  | .tyArr A B    => .tyArr (.shift A) (.shift B)
+  | .tyForall A   => .tyForall (.shift A)
+
+def Ty.subst {Δ} (U : Ty Δ) : Ty (Δ + 1) → Ty Δ
+  | .tyVar x      =>
+    match x with
+      | 0         => U
+      | ⟨k+1, hk⟩ => .tyVar ⟨k, Nat.lt_of_succ_lt_succ hk⟩
+  | .tyNat        => .tyNat
+  | .tyArr A B    => .tyArr (.subst U A) (.subst U B)
+  | .tyForall A   => .tyForall (.subst (.shift U) A)
+
+def Ty.ext {Δ Δ'}
+  (ρ : Fin Δ → Fin Δ')
+  : Fin (Δ + 1) → Fin (Δ' + 1)
+:= fun x => match x with
+  | 0         => 0
+  | ⟨k+1, hk⟩ => Fin.succ (ρ ⟨k, by omega⟩)
+
+def Ty.rename {Δ Δ'}
+  (ρ : Fin Δ → Fin Δ')
+  : Ty Δ → Ty Δ'
+:= fun A => match A with
+  | .tyVar x      => .tyVar (ρ x)
+  | .tyNat        => .tyNat
+  | .tyArr A B    => .tyArr (Ty.rename ρ A) (Ty.rename ρ B)
+  | .tyForall A   => .tyForall (Ty.rename (fun x => match x with
+      | ⟨0, _⟩    => 0
+      | ⟨k+1, hk⟩ => Fin.succ (ρ ⟨k, by omega⟩)
+    ) A)
+
+inductive Term : (Δ : Nat) → List (Ty Δ) → Ty Δ → Type u where
+  | var {Δ Γ A} :
+      Γ ∋ A
+    ----------
+    → Term Δ Γ A
+  | lam {Δ Γ B} :
+      (A : Ty Δ) → Term Δ (A :: Γ) B
+    ---------------------------------
+    → Term Δ Γ (Ty.tyArr A B)
+  | app {Δ Γ A B} :
+      Term Δ Γ (Ty.tyArr A B) → Term Δ Γ A
+    ---------------------------------------
+    → Term Δ Γ B
+  | tlam {Δ Γ A} :
+      Term (Δ + 1) (Γ.map Ty.shift) A
+    --------------------------------
+    → Term (Δ) Γ (Ty.tyForall A)
+  | tapp {Δ Γ A} :
+      Term Δ Γ (Ty.tyForall A) → (U : Ty Δ)
+    ---------------------------------------
+    → Term Δ Γ (Ty.subst U A)
+  | mu {Δ Γ A} :
+      Term Δ (A :: Γ) A
+    -------------------
+    → Term Δ Γ A
+  | succ {Δ Γ} : Term Δ Γ Ty.tyNat → Term Δ Γ Ty.tyNat
+  | zero {Δ Γ} : Term Δ Γ Ty.tyNat
+  | cases {Δ Γ A} :
+      Term Δ Γ Ty.tyNat → Term Δ Γ A → Term Δ (Ty.tyNat :: Γ) A
+      ---------------------------------------------------------
+      → Term Δ Γ A
+
+
+notation:60 " λ:" ty:0 "." e:50 => Term.lam ty e
+notation:55 e1:50 " ⋅ " e2:55 => Term.app e1 e2
+notation:60 " case " e1:60 "{zero=>" e2:60 ",succ=>" e3:60 " }" => Term.cases e1 e2 e3
+notation:60 " Λ. " e:60 => Term.tlam e
+notation:55 e " ⟨ " U:55 " ⟩ " => Term.tapp e U
+prefix:90 " ′ " => Term.var
---
+++ LearningLC/SystemF/Eval.lean
@@ -0,0 +1,29 @@
+import LearningLC.SystemF.Basic
+
+open Term Ty
+
+def exts {Δ Γ Δ' Γ'}
+  (Ρ : Fin Δ → Fin Δ')
+  (ρ : ∀ {A}, Γ ∋ A → Γ' ∋ (Ty.rename Ρ A))
+  ---------------------------------------
+  : ∀ {B A}, (B :: Γ) ∋ A → (Ty.rename Ρ B :: Γ') ∋ (Ty.rename Ρ A)
+:= fun x => match x with
+  | Ix.here => Ix.here
+  | Ix.there v => Ix.there (ρ v)
+
+def rename {Δ Γ Δ' Γ'}
+  (Ρ : Fin Δ → Fin Δ')
+  (ρ : ∀ {A}, Γ ∋ A → Γ' ∋ (Ty.rename Ρ A))
+  ------------------------------------------
+  : ∀ {A}, Term Δ Γ A → Term Δ' Γ' (Ty.rename Ρ A)
+:= fun M =>
+  match M with
+  | .var v       => .var (ρ v)
+  | .lam A t     => .lam (Ty.rename Ρ A) (rename Ρ (exts Ρ ρ) t)
+  | .app t1 t2   => .app (rename Ρ ρ t1) (rename Ρ ρ t2)
+  | .tlam t      => .tlam (rename (Ty.ext Ρ) (map_exts Ρ ρ) t)
+  | .tapp t U    => .tapp (rename Ρ ρ t) (Ty.rename Ρ U)
+  | .zero        => .zero
+  | .succ t      => .succ (rename Ρ ρ t)
+  | .cases e1 e2 e3 => .cases (rename Ρ ρ e1) (rename Ρ ρ e2) (rename Ρ (exts Ρ ρ) e3)
+  | .mu t        => .mu (rename Ρ (exts Ρ ρ) t)
